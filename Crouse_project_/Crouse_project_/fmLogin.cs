8230d9d6955959f9c6be800e57d5a048a2555d08
DOMINAtsu <116635239+DOMINAtsu@users.noreply.github.com>
2023-12-04 10:36:23 +0300

    Create fmLogin Forms and create fmMain forms

---
+++ Crouse_project_/Crouse_project_/fmLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crouse_project_
+{
+    public partial class fmLogin : Form
+    {
+        public fmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //Let create database and user table
+
+            if (MainClass.IsValidUser(txtUser.Text, txtPass.Text) == false)
+            {
+                guna2MessageDialog1.Show("invalid username or password");
+                return;
+            }
+            else
+            {
+                this.Hide();
+                frmMain frm = new frmMain();
+                frm.Show();
+            }
+
+            //let insert a user first
+        }
+
+
+
+
+
+    }
+}
